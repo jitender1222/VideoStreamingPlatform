429b6f9421b2432a40dbedde0788703e8cd852dd
jitender singh <jeetusinghkanyal@gmail.com>
2023-05-22 12:19:11 +0530

    added dependencies and created a Home Route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./component/Home/Home";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
