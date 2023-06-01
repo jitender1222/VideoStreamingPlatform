0cd6ca186c30e4928d786fe637b0f1d7ba379442
jitender singh <jeetusinghkanyal@gmail.com>
2023-06-02 00:52:43 +0530

    added User model and course Model

---
+++ backend/models/Course.js
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Please enter title"],
+    minLength: [4, "Title must be at least 4 characters"],
+    maxLength: [80, "Title can't exceed 80 characters"],
+  },
+
+  description: {
+    type: String,
+    required: [true, "Please enter the description"],
+    minLength: [20, "Title must be at least 20 characters"],
+  },
+
+  lectures: [
+    {
+      title: {
+        require: true,
+        type: String,
+      },
+      description: {
+        type: String,
+        required: true,
+      },
+      video: {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    },
+  ],
+  poster: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  numOfVideos: {
+    type: Number,
+    default: 0,
+  },
+  category: {
+    required: true,
+    type: string,
+  },
+  CreatedBy: {
+    required: [true, "Enter Course Creator Name"],
+    type: string,
+  },
+  CreatedAt: {
+    default: Date.now,
+    type: Date,
+  },
+});
+
+export const Course = mongoose.model("Course", schema);
