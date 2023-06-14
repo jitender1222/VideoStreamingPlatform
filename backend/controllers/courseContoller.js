406fd17677f3830f1cb33f14a26d671756282968
jitender singh <jeetusinghkanyal@gmail.com>
2023-06-14 23:58:44 +0530

    aaded

--- backend/controllers/courseContoller.js
+++ backend/controllers/courseContoller.js
@@ -3,9 +3,21 @@ import { Course } from "../models/Course.js";
 import getDataUri from "../utils/dataUri.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import cloudinary from "cloudinary";
+import { Stats } from "../models/Stats.js";
 
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
-  const getAll = await Course.find().select("-lectures");
+  const keyword = req.query.keyword || "";
+  const category = req.query.category || "";
+  const getAll = await Course.find({
+    title: {
+      $regex: keyword,
+      $options: "i",
+    },
+    category: {
+      $regex: category,
+      $options: "i",
+    },
+  }).select("-lectures");
   res.status(200).json({
     success: true,
     getAll,
@@ -141,4 +153,21 @@ export const deleteLecture = catchAsyncError(async (req, res, next) => {
     success: true,
     message: "Lecture deleted successfully",
   });
+});
+
+Course.watch().on("change", async () => {
+  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+
+  const courses = await Course.find({});
+
+  totalViews = 0;
+
+  for (let i = 0; i < courses.length; i++) {
+    totalViews += courses[i].views;
+  }
+
+  stats[0].views = totalViews;
+  stats[0].CreatedAt = new Date(Date.now());
+
+  await stats[0].save();
 });
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,7 @@ import crypto from "crypto";
 import { Course } from "../models/Course.js";
 import cloudinary from "cloudinary";
 import getDataUri from "../utils/dataUri.js";
+import { Stats } from "../models/Stats.js";
 
 // handle Register
 export const handlRegister = catchAsyncError(async (req, res, next) => {
@@ -311,4 +312,16 @@ export const deleteMyProile = catchAsyncError(async (req, res, next) => {
       success: true,
       message: "My profile deleted successfully",
     });
+});
+
+User.watch().on("change", async () => {
+  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+
+  const subscription = await User.find({ "subscription.status": "active" });
+
+  stats[0].users = await User.countDocuments();
+  stats[0].subscription = subscription.length;
+  stats[0].CreatedAt = new Date(Date.now());
+
+  await stats[0].save();
 });
