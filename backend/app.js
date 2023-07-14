0348fbb6dca72d56cd32dff22cb9684b74f62e37
jitender singh <jeetusinghkanyal@gmail.com>
2023-07-14 12:18:49 +0530

    added get request

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config({
   path: "./config/.env",
@@ -19,6 +20,14 @@ app.use(
 
 app.use(cookieParser());
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["POST", "GET", "PUT", "DELETE"],
+  })
+);
+
 // importing routes
 
 import course from "./routes/courseRoute.js";
@@ -32,6 +41,12 @@ app.use("/api/v1/course", course);
 app.use("/api/v1/user", user);
 app.use("/api/v1/payment", payment);
 app.use("/api/v1/other", otherRoutes);
+
+app.get("/", (req, res) => {
+  res.send(
+    `<h1>Site is working <a href=${process.env.FRONTEND_URL}>to visit the frontend</a>CLick</h1>`
+  );
+});
 
 // error middlewear
 app.use(errorMiddlewear);
--- backend/utils/sentToken.js
+++ backend/utils/sentToken.js
@@ -3,7 +3,7 @@ export const Token = (res, user, message, statusCode = 200) => {
   const options = {
     expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    // secure: true,
+    secure: true,
     sameSite: "none",
   };
   res.status(statusCode).cookie("token", token, options).json({
